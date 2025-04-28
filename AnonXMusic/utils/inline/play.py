8eb804964b451c7e28eaf7bad8f99ac5c267a559
TheRadhe <rajveer202415@gmail.com>
2025-04-28 10:51:48 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
 # Initialize your Pyrogram Client
-app = Client("your_bot", api_id=123456, api_hash="your_api_hash", bot_token="your_bot_token")
+app = Client("your_bot", api_id="14050586", api_hash="42a60d9c657b106370c79bb0a8ac560c", bot_token="7967697232:AAHSVO7F8xk3wOGOcPYiqxMnHl1oKWKzZrs")
 
 # ---------- Buttons ----------
 def stylish_stream_panel():
