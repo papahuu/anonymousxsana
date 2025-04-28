ca1a19b8b6c9a1bcfe65887679b2970a2cf02961
TheRadhe <rajveer202415@gmail.com>
2025-04-28 11:09:30 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
 # Initialize your Pyrogram Client
-app = Client("your_bot", 
+app = Client(""streamingking"", 
 api_id="14050586", 
 api_hash="42a60d9c657b106370c79bb0a8ac560c",
 bot_token="7967697232:AAHSVO7F8xk3wOGOcPYiqxMnHl1oKWKzZrs")
